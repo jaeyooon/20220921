bb0452b00891d2a89f5582a6492fb234c6fb62ae
jaeyooon <jycho0208@gmail.com>
2022-09-21 14:41:38 +0900

    2022-09-21 modify gittest

--- gittest/src/gittest/Test.java
+++ gittest/src/gittest/Test.java
@@ -3,7 +3,10 @@ package gittest;
 public class Test {
 
 	public static void main(String[] args) {
-
+		System.out.println("TEST");
+		System.out.println("TEST");
+		System.out.println("TEST");
+		System.out.println("TEST");
 	}
 
 }
